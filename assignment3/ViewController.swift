44c00b84063c32b70599bbec5a63ff2193204083
Frank <fzf0012@auburn.edu>
2023-06-11 11:29:04 -0400

    Initial Commit

---
+++ assignment3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment3
+//
+//  Created by Frank Fukuchi on 6/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
